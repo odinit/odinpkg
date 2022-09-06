668b4ec0b15633609f4f78993d59b5ede881da90
odinit <odinit@163.com>
2022-09-06 13:58:11 +0800

    odin:添加部分内容

--- otype/str.go
+++ otype/str.go
@@ -1,1 +1,35 @@
 package otype
+
+import "unicode"
+
+// IsStrContainSpecial 判断字符串中是否包含特殊字符
+func IsStrContainSpecial(s string) bool {
+	if s == "" {
+		return false
+	}
+
+	for _, c := range s {
+		if IsCharacterSpecial(c) {
+			return true
+		}
+	}
+	return false
+}
+
+func IsCharacterSpecial(c rune) bool {
+	return unicode.IsPunct(c) || unicode.IsSymbol(c) || unicode.Is(unicode.Han, c)
+}
+
+func IsStrAllLetterOrDigit(s string) bool {
+	if s == "" {
+		return false
+	}
+	for _, c := range s {
+		if unicode.IsDigit(c) || unicode.IsLetter(c) {
+			continue
+		} else {
+			return false
+		}
+	}
+	return true
+}
