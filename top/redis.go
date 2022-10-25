aa233cc6b804d4a60afce04ccba13c269073e869
odinit <odinit@163.com>
2022-10-25 10:34:24 +0800

    20221025:变更包名

--- top/redis.go
+++ top/redis.go
@@ -10,32 +10,21 @@ var (
 )
 
 // NewRedisClient 初始化连接
-func NewRedisClient(host string, port, db int, password string, opts ...*redis.Options) (client *redis.Client, err error) {
-	opt := new(redis.Options)
-	if len(opts) != 0 {
-		opt = opts[0]
-		if host != "" && port != 0 {
-			opt.Addr = fmt.Sprintf("%s:%d", host, port)
-		}
-		if db != 0 {
-			opt.DB = db
-		}
-		if password != "" {
-			opt.Password = password
-		}
-	} else {
-		opt.Addr = fmt.Sprintf("%s:%d", host, port)
-		opt.DB = db
-		opt.Password = password
+func NewRedisClient(host string, port, db int, password string, opts *redis.Options) (client *redis.Client, err error) {
+	if opts == nil {
+		opts = &redis.Options{}
 	}
+	opts.Addr = fmt.Sprintf("%s:%d", host, port)
+	opts.Password = password
+	opts.DB = db
 
-	client = redis.NewClient(opt)
+	client = redis.NewClient(opts)
 	_, err = client.Ping().Result()
 	return
 }
 
-func GlobalRedisClient(host string, port, db int, password string, opts ...*redis.Options) (err error) {
-	client, err := NewRedisClient(host, port, db, password, opts...)
+func GlobalRedisClient(host string, port, db int, password string, opts *redis.Options) (err error) {
+	client, err := NewRedisClient(host, port, db, password, opts)
 	if err != nil {
 		return
 	}
@@ -44,5 +33,5 @@ func GlobalRedisClient(host string, port, db int, password string, opts ...*redi
 }
 
 func TestRedisClient() (err error) {
-	return GlobalRedisClient("localhost", 6379, 0, "")
+	return GlobalRedisClient("localhost", 6379, 0, "", nil)
 }
